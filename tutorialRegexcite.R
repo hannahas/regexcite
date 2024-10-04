35f4a8d5ec3a6d429b90fb8a5939aee4cae4e459
hannahas <ahannah95@gmail.com>
2024-10-04 11:36:58 -0700

    Initial commit

--- tutorialRegexcite.R
+++ tutorialRegexcite.R
@@ -1,7 +1,6 @@
 # Create regexcite package using the Hadley Wickham tutorial
 library(devtools)
 create_package("/Users/ahannah/Desktop/rPackages/regexcite")
-use_git()
 usethis::use_git()
 
 load_all() # Makes strsplit1() available for experimentation; simulates the process of building, installing, and attaching the regexcite package
