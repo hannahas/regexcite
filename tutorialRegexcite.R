633543f982228f58d1ac623f6a33f9030f0b8722
hannahas <ahannah95@gmail.com>
2024-10-16 17:27:45 -0700

    Initial commit

--- tutorialRegexcite.R
+++ tutorialRegexcite.R
@@ -6,3 +6,27 @@ usethis::use_git()
 load_all() # Makes strsplit1() available for experimentation; simulates the process of building, installing, and attaching the regexcite package
 x <- "alfa,bravo,charlie,delta"
 strsplit1(x, split = ",")
+
+check() # Gives errors, warnings, notes
+
+use_mit_license() # Creates a LICENSE and LICENSE.md file
+
+# Next, modify strsplit1.R with 'Insert roxygen skeleton'.
+# Then convert tihs comment into man/strsplit1.Rd with document()
+document()
+
+# Preview help file
+?strsplit1
+
+check()
+
+# Install regexcite package into your library via install()
+install()
+
+# Now let's attach regexcite
+library(regexcite)
+x <- "alfa,bravo,charlie,delta"
+strsplit1(x, split = ",")
+
+# 1.15 use_testthat()
+use_testthat()
